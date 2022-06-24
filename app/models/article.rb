17989a51f88a89541707336c14161767c69624c7
Dipesh Gupta <dipesh.gupta@w3villa.com>
2022-06-24 17:43:30 +0530

    Active NavBar and Sticky Header

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
 
-  validates :title, presence:true ,uniqueness: true
-  validates :body, presence:true ,length:{minimum:100, maximum:1000}
+  validates :title, presence: true ,uniqueness: true
+  validates :body, presence: true ,length:{minimum:100, maximum:1000}
 end
